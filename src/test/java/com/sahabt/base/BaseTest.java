52b17d5ae5eec889534ee674e429f5d973f56dbe
ali.aktolun <ali.aktolun@sahabt.com>
2018-12-04 11:32:10 +0300

    revert base test

--- src/test/java/com/sahabt/base/BaseTest.java
+++ src/test/java/com/sahabt/base/BaseTest.java
@@ -1,47 +1,37 @@
 package com.sahabt.base;
 
-import com.sahabt.HomePage;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BaseTest {
 
-  private Logger logger = LoggerFactory.getLogger(getClass());
-  private final String URL = "http://hub.sahabt.io/wd/hub";
-  private RemoteWebDriver driver;
+  public static final String URL = "http://hub.testinium.io/wd/hub";
+  public static RemoteWebDriver driver;
 
   @Before
   public void setUp() {
     DesiredCapabilities capabilities = new DesiredCapabilities();
-    capabilities.setCapability("browserName", "chrome");
+    capabilities.setCapability("key", System.getProperty("key"));
     try {
-      capabilities.setCapability("key", System.getProperty("key"));
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    try {
-      driver = new RemoteWebDriver(new URL(URL), capabilities);
+      driver = new RemoteWebDriver(new URL(URL),capabilities);
     } catch (MalformedURLException e) {
       e.printStackTrace();
     }
-    driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    driver.manage().window().fullscreen();
-    driver.get("http://www.google.com/");
   }
 
   @Test
-  public void searchCase() {
-    HomePage homePage = new HomePage(driver);
-    homePage.search("sahabt");
+  public void testCase(){
+    navigate("http://www.hepsiburada.com/");
+  }
+
+  public void navigate(String url) {
+    driver.get(url);
   }
 
   @After
