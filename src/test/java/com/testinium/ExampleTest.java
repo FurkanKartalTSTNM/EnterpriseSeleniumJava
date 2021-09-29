c6c209eb13d3103daebd432c7ba96b4967e0bfd3
mehmetcantula <mehmetcan.tula@testinium.com>
2021-09-29 15:39:23 +0300

    test project updated

---
+++ src/test/java/com/testinium/ExampleTest.java
@@ -0,0 +1,14 @@
+package com.testinium;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest extends BaseTest {
+
+    Steps steps = new Steps();
+
+    @Test
+    public void exampleTest() {
+        steps.goToPage("https://testinium.com/");
+        steps.clickElement("random");
+    }
+}
