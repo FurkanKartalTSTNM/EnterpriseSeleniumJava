6ac69ace0654c799ec87d1124e1d73b8be5c8c2b
Furkan Kartal <Furkan.kartal@testinium.com>
2024-09-05 12:18:12 +0300

    deneme

--- src/test/java/com/testinium/ExampleTest.java
+++ src/test/java/com/testinium/ExampleTest.java
@@ -1,14 +1,34 @@
 package com.testinium;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class ExampleTest extends BaseTest {
 
-Steps steps = new Steps();
+    private WebDriver driver;
+    Steps steps = new Steps();
 
 @Test
 public void exampleTest() {
-    steps.goToPage("https://www.amazon.com.tr/");
-        steps.clickElement("nav-logo-sprites");
+    // Chrome driver başlatma
+    driver = new ChromeDriver();
+    driver.get("https://www.amazon.com.tr/");
+
+    // WebDriverWait kullanarak elementin görünmesini bekleme
+    WebDriverWait wait = new WebDriverWait(driver, 10);
+    WebElement logo = wait.until(ExpectedConditions.elementToBeClickable(By.id("nav-logo-sprites")));
+
+    // Elemente tıklama
+    logo.click();
+
+    // Tarayıcıyı kapatma
+    driver.quit();
     }
 }
