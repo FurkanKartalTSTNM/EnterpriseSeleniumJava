03c2754a57f4737a43dff3a47ee7a590db905926
TestiniumEngineeringQA <testinium-engineering-qa@testinium.com>
2025-04-09 01:39:54 +0300

    Addstep

--- src/test/java/com/testinium/ExampleTest.java
+++ src/test/java/com/testinium/ExampleTest.java
@@ -18,5 +18,21 @@ public void exampleTest() {
     steps.waitSeconds(2);
     driver.findElement(By.cssSelector(".a-button-close.a-declarative")).click();
     steps.waitSeconds(2);
+    driver.findElement(By.xpath("//a[1]")).click();
+    steps.waitSeconds(2);
+    driver.findElement(By.id("nav-logo-sprites")).click();
+    steps.waitSeconds(2);
+    driver.findElement(By.id("nav-global-location-popover-link")).click();
+    steps.waitSeconds(2);
+    driver.findElement(By.cssSelector(".a-button-close.a-declarative")).click();
+    steps.waitSeconds(2);
+    driver.findElement(By.xpath("//a[1]")).click();
+    steps.waitSeconds(2);
+    driver.findElement(By.id("nav-logo-sprites")).click();
+    steps.waitSeconds(2);
+    driver.findElement(By.id("nav-global-location-popover-link")).click();
+    steps.waitSeconds(2);
+    driver.findElement(By.cssSelector(".a-button-close.a-declarative")).click();
+    steps.waitSeconds(2);
     }
 }
